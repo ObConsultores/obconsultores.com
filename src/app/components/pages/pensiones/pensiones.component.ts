9d2632e8887d1113c2363e82544150647440a829
Gabriel Hernan Ordoñez Achury <gaher1096@hotmail.com>
2022-11-03 15:19:18 -0500

    Pensiones. Se implemento el componente de pensiones.

---
+++ src/app/components/pages/pensiones/pensiones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensiones',
+  templateUrl: './pensiones.component.html',
+  styleUrls: ['./pensiones.component.scss']
+})
+export class PensionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
