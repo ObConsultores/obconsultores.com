3578fbceb6fb7c2caa96ade90ee1b248bf43a670
Gabriel Hernan Ordoñez Achury <gaher1096@hotmail.com>
2022-11-04 08:00:23 -0500

    Otros servicios. Se implemento el componente de Otros servicios.

---
+++ src/app/components/pages/otros-servicios/otros-servicios.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OtrosServiciosComponent } from './otros-servicios.component';
+
+describe('OtrosServiciosComponent', () => {
+  let component: OtrosServiciosComponent;
+  let fixture: ComponentFixture<OtrosServiciosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OtrosServiciosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OtrosServiciosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
